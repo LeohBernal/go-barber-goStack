1ace1c706d78698f6867817b0a4e2baf79070edf
LeohBernal <leonardo.bernal@unesp.br>
2020-09-01 14:06:52 -0300

    Projetos em andamento

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '06e042ce955cce0ed99c93c1fcc2f34d',
+    expiresIn: '1d',
+  },
+};
---
+++ backend/src/modules/appointments/dtos/ICreateAppointmentDTO.ts
@@ -0,0 +1,4 @@
+export default interface ICreateAppointmentoDTO {
+  provider_id: string;
+  date: Date;
+}
