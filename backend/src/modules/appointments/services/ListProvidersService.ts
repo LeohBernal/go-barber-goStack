be73b28498255349911a617ccc0579993eaa4ed8
Leonardo Bernal <leonardo.bernal@unesp.br>
2020-12-07 14:17:01 -0300

    fixed list provider response

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -4,6 +4,7 @@ import User from '@modules/users/infra/typeorm/entities/User';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -23,14 +24,16 @@ class ListProvidersService {
     let users = await this.cacheProvider.recover<User[]>(
       `providers-list:${user_id}`,
     );
-
     if (!users) {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
       });
     }
 
-    await this.cacheProvider.save(`providers-list:${user_id}`, users);
+    await this.cacheProvider.save(
+      `providers-list:${user_id}`,
+      classToClass(users),
+    );
 
     return users;
   }
